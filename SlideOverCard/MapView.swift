30ef01068b15a8d9ea9717a56c9d4a0d2d85fc8f
Wanderson <lopez.wanderson@gmail.com>
2022-05-09 17:31:03 -0600

    Se crearon dos funciones para mostrar el mapa

--- SlideOverCard/MapView.swift
+++ SlideOverCard/MapView.swift
@@ -8,4 +8,15 @@
 import SwiftUI
 import MapKit
 
-
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: -33.523065, longitude: 151.394551)
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
