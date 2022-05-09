7f428beb6e794676aa8310a3f657de9b9eb7d8e9
Wanderson <lopez.wanderson@gmail.com>
2022-05-09 17:11:38 -0600

    Initial Commit

---
+++ SlideOverCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlideOverCard
+//
+//  Created by Wanderson López on 9/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlideOverCard/SlideOverCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideOverCardApp.swift
+//  SlideOverCard
+//
+//  Created by Wanderson López on 9/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SlideOverCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
